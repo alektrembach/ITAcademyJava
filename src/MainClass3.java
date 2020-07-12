06181af78e18f39349a8c0f4c12371c4aadf02f9
AlekTrembach <alextrembacz@gmail.com>
2020-07-12 16:18:46 +0200

    27 04 2020 Homework

--- src/MainClass3.java
+++ src/MainClass3.java
@@ -1,7 +1,7 @@
 public class MainClass3 {
 
     public static void main(String... args) {
-        System.out.println("Homework is done!")
-        {
+        System.out.println("Homework is done!");
+    }
 
 }
